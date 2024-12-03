3ad3913193f77ccd10e71d317ff8505e36a1a6c5
Abdelkarim Amro Eltaib Molina <103129948+some-du6e@users.noreply.github.com>
2024-12-03 15:11:54 -0600

    become a little bit more brainrotted

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,9 +1,36 @@
-function registerScript() {
+function runbash(text) {
   const textinput = document.getElementById('text')
-   console.log(textinput.value);
-   browser.runtime.sendMessage(textinput.value);
+   console.log(text);
+   browser.runtime.sendMessage(text);
   }
 
+function save(list) {
+  var storage = browser.storage.local.get("hawktuah")
+  var thong = browser.storage.local.set({ "hawktuah" : list });
+  console.log(storage)
+  thong.then(() => {
+    console.log(storage)
+  });
+  }
 
-const button = document.getElementById('button')
-button.addEventListener('click', registerScript)
+function getstorage() {
+  var storage = browser.storage.local.get("hawktuah")
+  storage.then((res) => {
+    console.log(res.hawktuah[0])
+    return res.hawktuah
+  });
+  }
+
+  function runprofile(profile) {
+    runbash("firefox -P" + profile)
+  }
+
+  function addprofiles(){
+    var profiles = getstorage()
+    profiles.then((profileList) => {
+      for (let i = 0; i < profileList.length; i++) {
+      console.log(profileList[i]);
+      }
+    });
+  }
+document.addEventListener('DOMContentLoaded', getstorage());
