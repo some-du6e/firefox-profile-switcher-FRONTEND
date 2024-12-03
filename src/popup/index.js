93bd3f0973907eec2d2b3d16b4bf0e42a950130d
Abdelkarim Amro Eltaib Molina <103129948+some-du6e@users.noreply.github.com>
2024-12-03 16:03:53 -0600

    refactor popup layout and enhance functionality with settings button

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,36 +1,42 @@
 function runbash(text) {
-  const textinput = document.getElementById('text')
-   console.log(text);
-   browser.runtime.sendMessage(text);
-  }
+  const textinput = document.getElementById('text');
+  console.log(text);
+  browser.runtime.sendMessage(text);
+}
 
 function save(list) {
-  var storage = browser.storage.local.get("hawktuah")
-  var thong = browser.storage.local.set({ "hawktuah" : list });
-  console.log(storage)
+  var storage = browser.storage.local.get('hawktuah');
+  var thong = browser.storage.local.set({ hawktuah: list });
+  console.log(storage);
   thong.then(() => {
-    console.log(storage)
+    console.log(storage);
   });
-  }
+}
 
 function getstorage() {
-  var storage = browser.storage.local.get("hawktuah")
+  var storage = browser.storage.local.get('hawktuah');
   storage.then((res) => {
-    console.log(res.hawktuah[0])
-    return res.hawktuah
+    console.log(res.hawktuah[0]);
+    return res.hawktuah;
   });
-  }
+}
 
-  function runprofile(profile) {
-    runbash("firefox -P" + profile)
-  }
+function runprofile(profile) {
+  runbash('firefox -P' + profile);
+}
 
-  function addprofiles(){
-    var profiles = getstorage()
-    profiles.then((profileList) => {
-      for (let i = 0; i < profileList.length; i++) {
+function addprofiles() {
+  var profiles = getstorage();
+  profiles.then((profileList) => {
+    for (let i = 0; i < profileList.length; i++) {
       console.log(profileList[i]);
-      }
-    });
-  }
+    }
+  });
+}
 document.addEventListener('DOMContentLoaded', getstorage());
+function confirmReset() {
+  if (!confirm('Press cancel to reset')) {
+    let clearing = browser.storage.local.clear();
+    alert(clearing);
+  }
+}
